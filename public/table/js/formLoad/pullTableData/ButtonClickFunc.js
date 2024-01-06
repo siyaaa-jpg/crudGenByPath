dfe7d546817a77ace57d57457df4f5d19b238875
keshavsoft <nknnkumar@yahoo.com>
2024-01-06 17:45:17 +0530

    keshav

--- public/table/js/formLoad/pullTableData/ButtonClickFunc.js
+++ public/table/js/formLoad/pullTableData/ButtonClickFunc.js
@@ -7,11 +7,30 @@ let StartFunc = async () => {
 };
 
 let LocalFetchFiles = async () => {
-    let jVarLocalFetchUrl = "/api/customers"
+    let jVarLocalTableName = LocalFuncForQueryParams({ inKey: "fileName" });
+    console.log("jVarLocalTableName : ", jVarLocalTableName);
+    let jVarLocalFetchUrl = `/api/${jVarLocalTableName.split(".")[0]}`;
     let response = await fetch(jVarLocalFetchUrl);
     let data = await response.json();
 
     return data;
+};
+
+let LocalFuncForQueryParams = ({ inKey }) => {
+    let jVarLocalKey = inKey;
+
+    const url = new URL(window.location.href);
+
+    // Get the search parameters from the URL
+    const params = new URLSearchParams(url.search);
+
+    // Get the value of a specific parameter
+    const parameterValue = params.get(jVarLocalKey);
+
+    return parameterValue;
+    // const queryString = window.location.search;
+    // const parameters = new URLSearchParams(queryString);
+    // const value = parameters.get(jVarLocalKey);
 };
 
 export { StartFunc };
