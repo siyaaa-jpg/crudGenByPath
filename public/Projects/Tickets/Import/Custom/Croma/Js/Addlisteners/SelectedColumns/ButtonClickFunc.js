3249ef8d1abc0f5a330dac31d738bc263ac712a1
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2024-01-08 20:15:25 +0530

    SREENIVAS

--- public/Projects/Tickets/Import/Custom/Croma/Js/Addlisteners/SelectedColumns/ButtonClickFunc.js
+++ public/Projects/Tickets/Import/Custom/Croma/Js/Addlisteners/SelectedColumns/ButtonClickFunc.js
@@ -1,7 +1,7 @@
 import { StartFunc as StartFuncCheckBeforeFetch } from "./CheckBeforeFetch.js";
 import { StartFunc as StartFuncAfterFetch } from "./AfterFetch.js";
 import { StartFunc as StartFuncFetchFunc } from "./FetchFunc.js";
-import { StartFunc as StartFuncPreparePostData } from "./PreparePostData.js";
+// import { StartFunc as StartFuncPreparePostData } from "./PreparePostData.js";
 
 
 let StartFunc = async () => {
@@ -16,7 +16,7 @@ let StartFunc = async () => {
             reader.onload = async function (e) {
                 const csvData = e.target.result;
                 const jsonArray = convertCsvToJsonFunction(csvData);
-                let jVarLocalBodyData = await StartFuncPreparePostData({ inCsvJsonData: jsonArray });
+                // let jVarLocalBodyData = await StartFuncPreparePostData({ inCsvJsonData: jsonArray });
                 let response = await StartFuncFetchFunc({ inBodyData: jsonArray });
 
                 StartFuncAfterFetch({ inFromFetch: response });
@@ -35,10 +35,10 @@ let convertCsvToJsonFunction = (csvData) => {
     console.log("parsedData:", parsedData);
 
     const selectedData = parsedData.data.map(({
-        'ExternalReferenceID': Jobno, 'Servify Call Creation Date': CreationDate, 'Servify Status': Status,
+        'ExternalReferenceID': JobId, 'Servify Call Creation Date': CreationDate, 'Servify Status': Status,
         'Customer Name': CustomerName, 'Customer Address': Address, 'Customer Mobile No': MobileNo,
         'Customer Alternate Mobile No.': ContactNo, 'Product Name': ModelName, 'Delivery Type': JobClassification, 'Product Category': ProductGroupName, 'Warranty': WarrantyType, 'SubServiceType': JobType, 'PinCode': Distancetype, 'Last Visit Remarks': AgentRemarks, 'Created By': DealerName, CallFromNo, Brand
-    }) => ({ Jobno, CreationDate, Status, CustomerName, Address, MobileNo, ContactNo, ModelName, JobClassification, ProductGroupName, WarrantyType, JobType, Distancetype, AgentRemarks, DealerName, CallFromNo, Brand }));
+    }) => ({ JobId, CreationDate, Status, CustomerName, Address, MobileNo, ContactNo, ModelName, JobClassification, ProductGroupName, WarrantyType, JobType, Distancetype, AgentRemarks, DealerName, CallFromNo, Brand }));
 
     return selectedData;
 }
