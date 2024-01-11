8486e9aa21f89066e8cc62b93a42885ca96be631
keshavsoft <nknnkumar@yahoo.com>
2024-01-11 12:56:42 +0530

    keshav

--- bin/routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-var router = express.Router();
-
-import { router as routerFromFolder } from './Tickets/route.js';
-
-router.use('/Tickets', routerFromFolder);
-
-export { router };
